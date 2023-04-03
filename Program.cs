5b68a511af0104bc332a275389a2479610f7b037
323 <323@323-1>
2023-04-03 14:17:06 +0300

    Добавлено приветственное сообщение

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Программа рассчитывает значение введенного числа по формуле: (Sin(число) - Math.Cos(число)) / 5");
 Console.Write("Введите значение параметра x: ");
 double x = double.Parse(Console.ReadLine());
 double result = (Math.Sin(x) - Math.Cos(x)) / 5;

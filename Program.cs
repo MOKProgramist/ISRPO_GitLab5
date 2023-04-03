61f42864571b711b9ba2cbbdf6fd8eda64967642
323 <323@323-1>
2023-04-03 14:27:05 +0300

    Добавлена проверка на введеное значение и выброс ошибки

--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Программа рассчитывает значение введенного числа по формуле: (Sin(число) - Math.Cos(число)) / 5");
 Console.Write("Введите значение параметра x: ");
-double x = double.Parse(Console.ReadLine());
+double x;
+bool isXDouble = double.TryParse(Console.ReadLine(), out x);
+if (!isXDouble)
+{
+    Console.WriteLine("Введеное значение не является числом.");
+    Console.ReadLine();
+} 
 double result = (Math.Sin(x) - Math.Cos(x)) / 5;
 
 Console.WriteLine($"Результат выполнения функции: {result}");

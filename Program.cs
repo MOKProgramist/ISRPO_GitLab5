fb6acb52b92d764e33b3bf2696dda6d45a394b4c
323 <323@323-1>
2023-04-03 14:13:13 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите значение параметра x: ");
+double x = double.Parse(Console.ReadLine());
+double result = (Math.Sin(x) - Math.Cos(x)) / 5;
+
+Console.WriteLine($"Результат выполнения функции: {result}");
+Console.ReadLine();
